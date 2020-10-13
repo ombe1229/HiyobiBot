f56eccb42b1cd7725905917219f9b68122113e98
ombe1229 <h3236516@gmail.com>
2020-10-13 23:37:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ async def on_ready():
     game = discord.Game(';help')
     await bot.change_presence(status=discord.Status.online, activity=game)
 
-@bot.command
-async def prefix+'help'(ctx):
+@bot.command()
+async def help(ctx):
     embed = discord.Embed(title=':ticket: HiyobiBot 도움말', color=0xababab)
     embed.set_thumbnail(url=thumbnail)
     embed.add_field(name=' - ' + prefix + '정보 [N]', value='망가 정보를 불러옵니다.', inline=True)
