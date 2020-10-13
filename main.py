df046965fb5c3f24591a9ff520016dcf30113b18
ombe1229 <h3236516@gmail.com>
2020-10-13 22:28:40 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+
+client = discord.Client()
+prefix = ';'
+thumbnail = 'https://i.imgur.com/GKPAp4q.png'
+
+class HiyobiBot(discord.Client):
+    async def on_ready(self):
+        print('히요비봇 온라인.')
+        game = discord.Game(';help')
+        await client.change_presence(status=discord.Status.online, activity=game)
+
+    async def on_message(self, message):
+        channel = message.channel
+
+        if message.author.bot:
+            return None
+
+        if message.content == prefix+'help':
+            embed = discord.Embed(title=':ticket: HiyobiBot 도움말', color=0xababab)
+            embed.set_thumbnail(url=thumbnail)
+            embed.add_field(name=' - '+prefix+'정보 [N]', value='망가 정보를 출력합니다.', inline=True)
+
+            await channel.send(embed=embed)
+            return None
+        elif message.content.startswith(prefix):
+            await channel.send('이해할 수 없는 명령어입니다.')
+            return None
+
+if __name__ == '__main__':
+    client = HiyobiBot()
+    client.run('NzY1NTU3MTM3ODMyNTQyMjA4.X4WipQ.t6XPIzTbgvOOSqVW6M0xbTkJINE')
