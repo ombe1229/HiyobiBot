e36b115d139035b12d38e9275fc4bb0f617bdd1a
ombe1229 <h3236516@gmail.com>
2020-10-13 23:35:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
 import discord
+from discord.ext import commands
 import requests
 import os
 
-client = discord.Client()
-prefix = ';'
+
+bot = commands.Bot(command_prefix=';')
 thumbnail = 'https://i.imgur.com/GKPAp4q.png'
 
-class HiyobiBot(discord.Client):
-    async def on_ready(self):
-        print('히요비봇 온라인.')
-        game = discord.Game(';help')
-        await client.change_presence(status=discord.Status.online, activity=game)
 
-    async def on_message(self, message, args):
-        channel = message.channel
-        args = message.content.s
+@bot.event
+async def on_ready():
+    print('히요비봇 온라인')
+    game = discord.Game(';help')
+    await bot.change_presence(status=discord.Status.online, activity=game)
 
-        if message.author.bot:
-            return None
+@bot.command
+async def prefix+'help'(ctx):
+    embed = discord.Embed(title=':ticket: HiyobiBot 도움말', color=0xababab)
+    embed.set_thumbnail(url=thumbnail)
+    embed.add_field(name=' - ' + prefix + '정보 [N]', value='망가 정보를 불러옵니다.', inline=True)
 
-        if message.content == prefix+'help':
-            embed = discord.Embed(title=':ticket: HiyobiBot 도움말', color=0xababab)
-            embed.set_thumbnail(url=thumbnail)
-            embed.add_field(name=' - ' + prefix + '정보 [N]', value='망가 정보를 불러옵니다.', inline=True)
+    await ctx.send(embed)
 
-            await channel.send(embed=embed)
-            return None
-        elif message.content.startswith(prefix):
-            await channel.send('이해할 수 없는 명령어입니다.')
-            return None
-
-if __name__ == '__main__':
-    client = HiyobiBot()
-    client.run(os.environ['token'])
+bot.run()
